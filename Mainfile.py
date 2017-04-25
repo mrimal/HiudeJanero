b8a6830fc27ee6d43ed65b404981bbcd43a8b93c
mrimal <mrimal@worldbank.org>
2017-04-25 12:41:26 -0400

    GoogleAPI

--- Mainfile.py
+++ Mainfile.py
@@ -11,4 +11,21 @@ Created on Tue Apr 25 10:46:08 2017
     #Data validation check using the provided shape files.
     #Data validation can be done at a municipal level rather than state level.
         #List of errors saved as 'address',[lat,long] pairs. 
-    
+
+import googlemaps
+from datetime import datetime
+import paths
+
+
+gmaps = googlemaps.Client(key=paths.API)
+
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1818 H St NW')
+
+print(geocode_result)
+# Look up an address with reverse geocoding
+#reverse_geocode_result = gmaps.reverse_geocode((, ))
+
+# Request directions via public transit
+now = datetime.now()
