652d3aad896f1565be5833cfce749ebb4a2b2af9
mrimal <mrimal@worldbank.org>
2017-04-25 13:57:23 -0400

    Using geopy instead of GMaps Library

--- Mainfile.py
+++ Mainfile.py
@@ -12,20 +12,37 @@ Created on Tue Apr 25 10:46:08 2017
     #Data validation can be done at a municipal level rather than state level.
         #List of errors saved as 'address',[lat,long] pairs. 
 
-import googlemaps
-from datetime import datetime
+#import googlemaps
+from geopy.geocoders import GoogleV3
+#from datetime import datetime
 import paths
+import glob
+import pandas as pd
+from pandas import DataFrame
 
+geocode = []
+
+geolocator = GoogleV3(api_key=paths.API)
 
 gmaps = googlemaps.Client(key=paths.API)
 
-
-# Geocoding an address
-geocode_result = gmaps.geocode('1818 H St NW')
-
-print(geocode_result)
-# Look up an address with reverse geocoding
-#reverse_geocode_result = gmaps.reverse_geocode((, ))
-
-# Request directions via public transit
-now = datetime.now()
+for each in glob.iglob(paths.filename):
+    # Geocoding an address
+    
+    municipal_code = each
+    df_mc = pd.read_csv(municipal_code,sep=',',encoding = 'latin1')
+    streetnames = df_mc['addr']
+    for streets in streetnames:
+        location = gmaps.geocode(streets, timeout = 10)
+   # print((location.latitude, location.longitude))
+        if location:
+            geocode.append(location)
+            print((location.latitude, location.longitude)) 
+     # Look up an address with reverse geocoding
+    #reverse_geocode_result = gmaps.reverse_geocode((, ))
+    
+    # Request directions via public transit
+final_table = pd.DataFrame(geocode, index=None)
+writer = pd.ExcelWriter('ShortOutput.xlsx')
+final_table.to_excel(writer, 'Sheet1' )
+writer.save()
