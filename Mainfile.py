261130e7f5443b3ede8f4c88fed788c78c0b4ab1
mrimal <mrimal@worldbank.org>
2017-04-25 10:51:53 -0400

    Initial Commit

---
+++ Mainfile.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 10:46:08 2017
+
+@author: mpr
+"""
+
+#Things to Do:
+    #Geoocde the addresses using a couple of different APIs.
+        #Save addresses that were not geocoded. 
+    #Data validation check using the provided shape files.
+    #Data validation can be done at a municipal level rather than state level.
+        #List of errors saved as 'address',[lat,long] pairs. 
+    
