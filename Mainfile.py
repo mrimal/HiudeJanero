b71414224bb7d2a342cc6fba9a919955985e161a
mrimal <mrimal@worldbank.org>
2017-05-03 16:19:28 -0400

    Testing different street address styles

--- Mainfile.py
+++ Mainfile.py
@@ -22,17 +22,27 @@ from pandas import DataFrame
 
 geocode = []
 geocodeError = [] 
+geocode1 = []
+geocodeError1 = []
 geolocator = GoogleV3(api_key=paths.API)
 
 #gmaps = googlemaps.Client(key=paths.API)
 
 for each in glob.iglob(paths.fileLocNew):
     # Geocoding an address
-    print(each)
+    #print(each)
     
-    df_mc = pd.read_csv(each)
-    print(df_mc)
-    streetnames = df_mc['street']
+    df_mc = pd.read_csv(each)[1:250]
+    
+    #dfShortened = df_mc.ix[:,1:250]
+    
+    #print(dfShortened)
+    
+    streetnames = df_mc['street'] 
+    streetNeigh = df_mc['street'] + "," +  df_mc['neighboorhood']
+    print(streetNeigh)   
+   
+    
     for streets in streetnames:
         location = geolocator.geocode(streets, timeout = 10)
    # print((location.latitude, location.longitude))
@@ -45,7 +55,42 @@ for each in glob.iglob(paths.fileLocNew):
     #reverse_geocode_result = gmaps.reverse_geocode((, ))
     
     # Request directions via public transit
+    for total in streetNeigh:
+        location = geolocator.geocode(total, timeout = 10)
+   # print((location.latitude, location.longitude))
+        if location:
+            geocode1.append(location)
+            print((location.latitude, location.longitude)) 
+        else:
+            geocodeError1.append(total)
+            
 final_table = pd.DataFrame(geocode, index=None)
 writer = pd.ExcelWriter('ShortOutput.xlsx')
 final_table.to_excel(writer, 'Sheet1' )
 writer.save()
+
+errorTable = pd.DataFrame(geocodeError, index=None)
+writerError = pd.ExcelWriter('ErrorOutput.xlsx')
+errorTable.to_excel(writerError, 'Sheet1')
+writer.save()
+
+            
+final_table = pd.DataFrame(geocode1, index=None)
+writer = pd.ExcelWriter('ShortOutput1.xlsx')
+final_table.to_excel(writer, 'Sheet1' )
+writer.save()
+
+errorTable = pd.DataFrame(geocodeError1, index=None)
+writerError = pd.ExcelWriter('ErrorOutput1.xlsx')
+errorTable.to_excel(writerError, 'Sheet1')
+writer.save()
+
+'''
+def saveTabletoExcel(listName, fileName):
+    final_table = pd.DataFrame(listName, index=None)
+    writer = pd.ExcelWriter(filename)
+    final_table.to_excel(writer, 'Sheet1')
+    writer.save()
+
+saveTabletoExcel(streetnames, street)
+'''
