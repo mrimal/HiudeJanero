12c2f109b8e1fc78f75fd8a8a0c8c98c3cc406bb
mrimal <mrimal@worldbank.org>
2017-05-18 14:23:47 -0400

    commit 2

--- anotherTry.py
+++ anotherTry.py
@@ -2,35 +2,36 @@
 """
 Created on Thu May 18 10:03:48 2017
 
-@author: mrijan
+@author: mpr
 """
 
-    import fiona
-    import shapely
-    from shapely.geometry import Point
+import fiona
+import shapely
+from shapely.geometry import Point
+import paths
     
-    with fiona.open(paths.shapefilepath) as fiona_collection:
+with fiona.open(paths.shapefilePath) as fiona_collection:
     
-        # In this case, we'll assume the shapefile only has one record/layer (e.g., the shapefile
-        # is just for the borders of a single country, etc.).
-        shapefile_record = fiona_collection.next()
-        #print(shapefile_record)
-        # Use Shapely to create the polygon
-        shape = shapely.geometry.asShape(shapefile_record[''])
-        print(shape)
-        #point = shapely.geometry.Point(-43.414, -22.392) # longitude, latitude
-        point = Point(-43.0302544, -22.7920041) # longitude, latitude
-        print(point)
+    # In this case, we'll assume the shapefile only has one record/layer (e.g., the shapefile
+    # is just for the borders of a single country, etc.).
+    shapefile_record = fiona_collection.next()
+    #print(shapefile_record)
+    # Use Shapely to create the polygon
+    shape = shapely.geometry.asShape(shapefile_record['Geometry'])
+    print(shape)
+    #point = shapely.geometry.Point(-43.414, -22.392) # longitude, latitude
+    point = Point(-43.0302544, -22.7920041) # longitude, latitude
+    print(point)
         
         
-        # Alternative: if point.within(shape)
-        if shape.contains(point):
-            print "Found shape for point."
-        else:
-            print "What up"
+    # Alternative: if point.within(shape)
+    if shape.contains(point):
+        print "Found shape for point."
+    else:
+        print "What up"
+                
+    if point.within(shape):
+        print "Yo yo yo"
+    else:
+        print "what up myaan"
             
-        if point.within(shape):
-            print "Yo yo yo"
-        else:
-            print "what up myaan"
-        
