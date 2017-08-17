d54a48d7c383150e5cae30f4db1d7bc16ab12ab0
mrimal <mrijan00@hotmail.com>
2017-08-17 14:29:18 -0400

    Initial Commit

---
+++ fileexport.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 11:29:16 2017
+
+@author: mpr
+"""
+
+import pandas as pd
+import os
+
+
+def csv_export(listname, filename):
+    """
+    This gives us the lists we have created with addresses that passed and
+    failed as csvs. This can be improved by just defining csvexport(list) to
+    give us csv for any file. This would be one place where the code would
+    change in the final version.
+    """
+    final_table = pd.DataFrame(listname, index=None)
+    file = filename + '.csv'
+    filepath = os.path.join("output", file)
+    final_table.to_csv(filepath, encoding='utf-8')
---
+++ multipolygon.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 4 15:14:18 2017
+
+@author: mpr
+"""
+from __future__ import print_function
+import csv
+from shapely.geometry import Polygon, Point, MultiPolygon
+import shapefile
+import paths
+import fileexport
+
+path = paths.shapefilePath
+
+polygon = shapefile.Reader(path)
+
+polygon = polygon.shapes()
+
+shpfilePoints = [shape.points for shape in polygon]
+
+polygons = shpfilePoints
+
+goodlist = []
+#defining the function that sees if the files are available inside the point
+# or not.
+def findInsideOut(point, address):
+    """
+    This function finds whether or not
+    the coordinates given are inside the
+    boundaries of the shape file
+    and then appends it to a list so that
+    it can be written to a csv file.
+    """
+    for polygon in polygons:
+        poly = Polygon(polygon)
+        #print poly
+        if poly.contains(point):
+            print('inside')
+            x = [point, address, "inside"]
+            goodlist.append(x)
+
+def loopsandFind():
+    """
+    Using CSV reader to read the geocordinates from the earlier files and checking
+    to see if they fall inside or outside.
+    """
+    with open(paths.exportFile) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            latitude = float(row['0'])
+            longitude = float(row['1'])
+            address = row['2']
+            geocod = Point(longitude, latitude)
+            findInsideOut(geocod, address)
+
+def main():
+    """
+    runs both the looping and finding
+    function and the function to export
+    the csv from the fileexport imported
+    file
+    """
+    loopsandFind()
+    fileexport.csv_export(goodlist, "found_address")
+
+if __name__ == '__main__':
+    main()
+
+
