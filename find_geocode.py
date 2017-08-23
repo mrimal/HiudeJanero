190464ba2c7b6ddd7fef86fb3f3f4871bb7ddd9a
mrimal <mrijan00@hotmail.com>
2017-08-23 11:05:05 -0400

    removing duplicates

--- find_geocode.py
+++ find_geocode.py
@@ -25,7 +25,7 @@ def locate(x):
     lon = location.longitude
     try:
         #Get geocode
-        location = geolocator.geocode(x, timeout=8, exactly_one=True)
+        location = geolocator.geocode(x, timeout=10, exactly_one=True)
         lat = location.latitude
         lon = location.longitude
     except AttributeError:
@@ -40,8 +40,17 @@ def locate(x):
     return pd.Series([lat,  lon])
 
 def fileread(filepath):
+    """
+    Create a new dataframe that has the address, latitude, and longitude, 
+    and then apply the locate function to the dataframe, and fill in the 
+    lat/lon empty boxes with whatever the function returns.
+    
+    This should help us create one table with everything in it.
+    """
     df_addr = pd.read_csv(filepath)
     df_geo = pd.DataFrame(columns = ['BIG_ADDR', 'LAT', 'LON'])
     df_geo['BIG_ADDR'] = df = df_addr['street'] + "," + df_addr['neighboorhood'] + "," + \
-                                df_addr['municipal'] + "," + df_addr['state_name']
+                                df_addr['municipal'] + "," + df_addr['state_name'] + "," + \
+                                str(df_addr['zipcode'])
+    df_geo = df_geo['BIG_ADDR'].drop_duplicates().reset_index()
     return df_geo
